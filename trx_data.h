c94d7e05f5c3e6834fa6f5c4d99f395f175e6604
root <root@test.redhat>
2014-04-13 20:03:06 +0400

    Something to remember

--- trx_data.h
+++ trx_data.h
@@ -4,6 +4,9 @@
 typedef struct _ip_addr
 {
    __u32   addr;  // WARNING ipv6 must be 128 bit
+#ifdef __cplusplus
+   explicit _ip_addr(__u32   addr=0):addr(addr){};
+#endif
 } ip_addr_t;
 
 typedef struct filter_rule_base {
@@ -13,6 +16,9 @@ typedef struct filter_rule_base {
     __u16 proto;
     __u16 src_port;
     __u16 dst_port;
+#ifdef __cplusplus
+    explicit  filter_rule_base( __u16 proto=0, __u16 src_port=0, __u16 dst_port=0):proto(proto),src_port(src_port),dst_port(dst_port){}
+#endif
 } filter_rule_base_t;
 
 typedef struct filter_rule{
@@ -22,7 +28,10 @@ typedef struct filter_rule{
    __u8  off;	    
    __u8  direction;
    __u8  policy; 
-   __u32 id;	
+   __u32 id;
+#ifdef __cplusplus
+   filter_rule(__u16 proto=0, __u16 src_port=0, __u16 dst_port=0):base_rule(proto,src_port,dst_port){};
+#endif
 } filter_rule_t;
 
 #pragma pack ()
