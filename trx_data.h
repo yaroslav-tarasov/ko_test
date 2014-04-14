3e470a86d26be59ac9c159ec50a90a43abd4bb4b
root <root@test.centos>
2014-04-14 17:17:27 +0400

    To c++

--- trx_data.h
+++ trx_data.h
@@ -13,6 +13,9 @@ typedef struct filter_rule_base {
     __u16 proto;
     __u16 src_port;
     __u16 dst_port;
+#ifdef __cplusplus
+    filter_rule_base(__u16 proto, __u16 src_port, __u16 dst_port): proto(proto),src_port(src_port),dst_port(dst_port) {};
+#endif
 } filter_rule_base_t;
 
 typedef struct filter_rule{
@@ -23,6 +26,10 @@ typedef struct filter_rule{
    __u8  direction;
    __u8  policy; 
    __u32 id;	
+#ifdef __cplusplus
+   filter_rule(): base_rule(0,0,0) {};
+   filter_rule(__u16 proto, __u16 src_port, __u16 dst_port): base_rule(proto,src_port,dst_port) {};
+#endif
 } filter_rule_t;
 
 #pragma pack ()
