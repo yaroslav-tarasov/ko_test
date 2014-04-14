34174e3aee5ae227b53dc7a7270c7ddde06b6579
yaroslav-tarasov <yaroslav.tarasov@gmail.com>
2014-04-14 18:00:34 +0400

    For branch sclerosis

--- trx_data.h
+++ trx_data.h
@@ -4,6 +4,9 @@
 typedef struct _ip_addr
 {
    __u32   addr;  // WARNING ipv6 must be 128 bit
+#ifdef __cplusplus
+   explicit _ip_addr(__u32   addr=0):addr(addr){};
+#endif
 } ip_addr_t;
 
 typedef struct filter_rule_base {
@@ -14,7 +17,7 @@ typedef struct filter_rule_base {
     __u16 src_port;
     __u16 dst_port;
 #ifdef __cplusplus
-    filter_rule_base(__u16 proto, __u16 src_port, __u16 dst_port): proto(proto),src_port(src_port),dst_port(dst_port) {};
+    explicit  filter_rule_base( __u16 proto=0, __u16 src_port=0, __u16 dst_port=0):proto(proto),src_port(src_port),dst_port(dst_port){}
 #endif
 } filter_rule_base_t;
 
@@ -25,10 +28,9 @@ typedef struct filter_rule{
    __u8  off;	    
    __u8  direction;
    __u8  policy; 
-   __u32 id;	
+   __u32 id;
 #ifdef __cplusplus
-   filter_rule(): base_rule(0,0,0) {};
-   filter_rule(__u16 proto, __u16 src_port, __u16 dst_port): base_rule(proto,src_port,dst_port) {};
+   filter_rule(__u16 proto=0, __u16 src_port=0, __u16 dst_port=0):base_rule(proto,src_port,dst_port){};
 #endif
 } filter_rule_t;
 
@@ -38,4 +40,3 @@ enum {POLICY_DROP,POLICY_ACCEPT};
 enum {IPPROTO_NOTEXIST=65000};
 enum {MSG_ADD_RULE=NLMSG_MIN_TYPE + 2,MSG_DATA,MSG_DONE,MSG_RULE_ERR,MSG_DELETE_RULE ,MSG_DELETE_ALL_RULES,MSG_UPDATE_RULE,MSG_GET_RULES,MSG_OK};
 
- 
